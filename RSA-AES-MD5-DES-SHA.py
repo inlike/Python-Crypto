b87a149ade0a226ae536e474641fb8ca4e1a18a4
billl <billl@synnex.com>
2018-12-26 17:47:09 +0800

    2018-12-26

--- RSA-AES-MD5-DES-SHA.py
+++ RSA-AES-MD5-DES-SHA.py
@@ -1,6 +1,6 @@
 # -*- coding：utf-8 -*-
 import base64
-
+import rsa
 from Crypto.Cipher import AES
 
 
@@ -42,81 +42,47 @@ class USE_AES:
         return str(aes.decrypt(base64.decodebytes(bytes(text, encoding='utf8'))).rstrip(b'\0').decode("utf8"))  # 解密
 
 
+class USE_RSA:
+    """
+    生成密钥可保存.pem格式文件
+    1024位的证书，加密时最大支持117个字节，解密时为128；
+    2048位的证书，加密时最大支持245个字节，解密时为256。
+    https://blog.csdn.net/MTbaby/article/details/80453687
+    """
+    def __init__(self, number=1024):
+        self.pubkey, self.privkey = rsa.newkeys(number)
+
+    def rsaEncrypt(self, text):
+        """
+
+        :param test: str
+        :return: bytes
+        """
+        content = text.encode('utf-8')
+        crypto = rsa.encrypt(content, self.pubkey)
+        return crypto
+    
+    def rsaDecrypt(self, text):
+        """
+        
+        :param text:bytes 
+        :return: str
+        """
+        content = rsa.decrypt(text, self.privkey)
+        con = content.decode('utf-8')
+        return con
+        
+    def savePem(self, path_name, text):
+        if "PEM" in path_name.upper():
+            path_name = path_name[:-4]
+        with open('{}.pem'.format(path_name), 'w') as f:
+            f.write(text)
+
+
 if __name__ == '__main__':
-    aes_test = USE_AES("assssssssdfasasasasa")
-    a = aes_test.encrypt("测试")
-    b = aes_test.decodebytes(a)
-
-
-import hashlib
-
-
-def hash_demo():
-    m = hashlib.md5()
-    m.update(b"hello")
-    m.update(b"world!")  # = hello + world!
-
-    hash_hex = hashlib.sha3_512(b"luzhuo.me").hexdigest()
-
-    print(m.digest_size)
-    print(m.digest())  # 二进制hash
-    print(m.hexdigest())  # 十六进制hash
-    print(hash_hex)
-
-    # 加盐加密
-    hash_bytes = hashlib.pbkdf2_hmac('sha256', b'luzhuo.me', b'80', 100000)
-    print(hash_bytes)
-
-
-def hash_func():
-    # hashlib.new(name[, data])  // 创建hashlib(非首选), name=算法名, data:数据
-    hash = hashlib.new('ripemd160', b'luzhuo.me')
-
-    # 常量
-    dics = hashlib.algorithms_guaranteed  # 所有平台支持的hash算法的名称
-    dics = hashlib.algorithms_available  # 在Python解析器中可用的hash算法的名称, 传递给new()时, 可识别
-
-    # hashlib.pbkdf2_hmac(hash_name, password, salt, iterations, dklen=None) // 加盐加密 hash_name:hash名称, password:数据, salt:盐, iterations:循环次数, dklen:密钥长度
-    hash_bytes = hashlib.pbkdf2_hmac('sha256', b'luzhuo.me', b'80', 100000)
-
-    # hash对象
-    num = hash.digest_size  # hash结果的大小
-    num = hash.block_size  # hash算法的内部块的大小
-    strs = hash.name  # hash名称, 可传给new()使用
-    hash.update(b"data")  # 字节缓冲区 hash.update(a) hash.update(b) == hash.update(a+b)
-    hash_bytes = hash.digest()  # 字节hash
-    hash_str = hash.hexdigest()  # 16进制字符串hash
-    hash = hash.copy()  # 拷贝hash对象副本
-
-
-import hmac
-
-
-def hmac_demo():
-    # 加密
-    h = hmac.new(b"net")
-    h.update(b"luzhuo.me")
-    h_str = h.hexdigest()
-    print(h_str)
-
-    # 比较密码
-    boolean = hmac.compare_digest(h_str, hmac.new(b"net", b"luzhuo.me").hexdigest())
-    print(boolean)
-
-
-
-def hmac_func():
-    # 创建key和内容,再都进行加密
-    # hmac.new(key, msg=None, digestmod=None) // 创建新的hmac对象, key:键, msg:update(msg), digestmod:hash名称(同hashlib.new())(默认md5)
-    hc = hmac.new(b"key")
-
-    # hmac对象
-    hc.update(b"msg")  # 字节缓冲区  hc.update(a) hc.update(b) == hc.update(a+b)
-    hash_bytes = hc.digest()  # 字节hash
-    hash_str = hc.hexdigest()  # 16进制hash字符串
-    hc = hc.copy()  # 拷贝hmac副本
-    num = hc.digest_size  # hash大小
-    num = hc.block_size  # hash算法内部块大小
-    strs = hc.name  # hash名称
-    # hmac.compare_digest(a, b) // 比较两个hash密钥是否相同, 参数可为: str / bytes-like object, (注:建议使用,不建议使用a==b)
-    boolean = hmac.compare_digest(hmac.new(b"net", b"luzhuo.me").digest(), hmac.new(b"net", b"luzhuo.me").digest())
+    # aes_test = USE_AES("assssssssdfasasasasa")
+    # a = aes_test.encrypt("测试")
+    # b = aes_test.decodebytes(a)
+    rsa_test = USE_RSA()
+    a = rsa_test.rsaEncrypt("测试加密")
+    b = rsa_test.rsaDecrypt(a)
