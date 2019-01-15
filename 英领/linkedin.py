719954f526d0e354aa8132c596a76debd7e76fc3
billl <billl@synnex.com>
2019-01-15 10:19:05 +0800

    完成linkedin

--- 英领/linkedin.py
+++ 英领/linkedin.py
@@ -7,17 +7,19 @@ session = requests.Session()
 
 
 def login(name, password):
-    session.headers= {"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-    "accept-encoding": "gzip, deflate, br",
-    "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
-    "upgrade-insecure-requests": "1",
-    "user-agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36",
-                         }
+    session.headers = {"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+                    "accept-encoding": "gzip, deflate, br",
+                    "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
+                    "upgrade-insecure-requests": "1",
+                    "user-agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 "
+                  "(KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36",
+                      }
     url = 'https://www.linkedin.com/'
-    login_url = 'https://www.linkedin.com/uas/login-submit?loginSubmitSource=GUEST_HOME'
+    login_url = 'https://www.linkedin.com/uas/login-submit?' \
+                'loginSubmitSource=GUEST_HOME'
     page = session.get(url)
     rp = etree.HTML(page.text)
-    loginCsrfParam = rp.xpath('//*[@id="loginCsrfParam-login"]/@value')[0]
+    loginCsrfParam = rp.xpath('//*[@name="loginCsrfParam"]/@value')[0]
 
     form_data = {
         'session_key': name,
@@ -30,6 +32,7 @@ def login(name, password):
 
     login_rp = etree.HTML(page_login.text)
     verify_url = 'https://www.linkedin.com/checkpoint/challenge/verify'
+    csrfToken = login_rp.xpath('//*[@name="csrfToken"]/@value')[0]
     pageInstance = login_rp.xpath('//*[@name="pageInstance"]/@value')[0]
     resendUrl = login_rp.xpath('//*[@name="resendUrl"]/@value')[0]
     challengeId = login_rp.xpath('//*[@name="challengeId"]/@value')[0]
@@ -42,7 +45,7 @@ def login(name, password):
     failureRedirectUri = login_rp.xpath('//*[@name="failureRedirectUri"]/@value')[0]
     pin = input('请输入验证码')
     verify_data = {
-        'verify_url': verify_url,
+        'csrfToken': csrfToken,
         'pageInstance': pageInstance,
         'resendUrl': resendUrl,
         'challengeId': challengeId,
@@ -59,4 +62,4 @@ def login(name, password):
 
 
 if __name__ == '__main__':
-    login('2551513277@qq.com', '123456lkj')
+    login('2551513277@qq.com', 'sdsdsdsd')
